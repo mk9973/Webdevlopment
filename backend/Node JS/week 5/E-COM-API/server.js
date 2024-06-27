37377be13b4c0813893a4dc658d39d4bed7804a4
mk9973 <mantoshkk367@gmail.com>
2024-06-27 13:45:37 +0530

    Swagger API 2.0 completed

--- backend/Node JS/week 5/E-COM-API/server.js
+++ backend/Node JS/week 5/E-COM-API/server.js
@@ -3,12 +3,17 @@
 
 //const express=require('express');    //common module
 import express from "express";   //ES6 module but you have to also add "type":"module" in json file
+import swagger from "swagger-ui-express";
 //import bodyParser from "body-parser";
 import productRouter from "./src/features/product/product.routes.js";
 import userRouter from "./src/features/user/user.routes.js";
 import cartRouter from "./src/features/cartItems/cartItems.routes.js";
 //import basicAuthorizer from "./src/middlewares/basicAuth.middleware.js";
 import jwtAuth from "./src/middlewares/jwt.middleware.js";
+//import apiDocs under swagger ui express
+//here only javascript module should imported but here it is json module so we have to explicitly type:'json' 
+import apiDocs from "./swagger.json" assert{type:'json'};
+
 const server=express();
 
 //server.use(bodyParser.json());  // for using this you have to install and import the parser
@@ -20,6 +25,8 @@ server.use("/api/products",jwtAuth,productRouter);
 server.use("/api/users",userRouter);
 server.use("/api/cartItems",jwtAuth,cartRouter);
 
+//first import swagger ui express
+server.use("/api-docs",swagger.serve, swagger.setup(apiDocs));
 server.get('/',(req,res)=>{
     return res.send("Welcome to E-commerce API ");
 });
