bc5da68a9e8b6ed101bcf8cb28b93ac5479ebde4
mk9973 <mantoshkk367@gmail.com>
2024-06-28 19:28:38 +0530

    completed loggers Middleware

--- backend/Node JS/week 5/E-COM-API/server.js
+++ backend/Node JS/week 5/E-COM-API/server.js
@@ -4,6 +4,7 @@
 //const express=require('express');    //common module
 import express from "express";   //ES6 module but you have to also add "type":"module" in json file
 import swagger from "swagger-ui-express";
+import cors from "cors";
 //import bodyParser from "body-parser";
 import productRouter from "./src/features/product/product.routes.js";
 import userRouter from "./src/features/user/user.routes.js";
@@ -14,20 +15,46 @@ import jwtAuth from "./src/middlewares/jwt.middleware.js";
 //here only javascript module should imported but here it is json module so we have to explicitly type:'json' 
 //import apiDocs from "./swagger2.0.json" assert{type:'json'};
 import apiDocs from "./swagger3.0.json" assert{type:'json'};
-
+//import logger middleware
+import loggerMiddleware from "./src/middlewares/logger.middleware.js";
 const server=express();
 
+//CORS policy configuration
+//here we are using cors library , it allow all origins,hearder,method etc.
+//first install -->  'npm i cors' and import
+server.use(cors());
+
+
+// CORS policy configuration
+// server.use((req, res, next)=>{
+//     //here if you add '*' --> instead of 'http://localhost:9971'then give access to all the web browser clients or origin
+//     res.header('Access-Control-Allow-Origin','http://localhost:9971');
+//     //here we allow all the headers using '*' if you allow specific header the you you can add 'Content-Type,Authorization')
+//     res.header('Access-Control-Allow-Headers','*');
+//     res.header('Access-Control-Allow-Methods','*');
+//     // return ok for preflight request.
+//     if(req.method=="OPTIONS"){
+//       return res.sendStatus(200);
+//     }
+//     next();
+//   })
+
+server.use(loggerMiddleware);
 //server.use(bodyParser.json());  // for using this you have to install and import the parser
 server.use(express.json());
+
 //for all requests related tp product, redirect to product routes.
 //localhost:9973/api/products
 
 server.use("/api/products",jwtAuth,productRouter);
 server.use("/api/users",userRouter);
-server.use("/api/cartItems",jwtAuth,cartRouter);
+server.use("/api/cartItems",jwtAuth,loggerMiddleware,cartRouter);
 
 //first import swagger ui express
 server.use("/api-docs",swagger.serve, swagger.setup(apiDocs));
+
+
+
 server.get('/',(req,res)=>{
     return res.send("Welcome to E-commerce API ");
 });
---
+++ backend/Node JS/week 5/E-COM-API/src/middlewares/logger.middleware.js
@@ -0,0 +1,63 @@
+
+// import fs from 'fs';
+
+// const fsPromise = fs.promises;
+// // fspromise is the object of promises it allow create and write data interfiles synchronouly without using callback
+// async function log(logData) {
+//     try {
+//         logData = `\n ${new Date().toString()} - ${logData}`;
+//         await fsPromise.appendFile(
+//             'log.txt', 
+//             logData
+//             );
+//     } catch(err) {
+//         console.log(err);
+//     }
+// }
+
+// const loggerMiddleware = async (
+//     req, 
+//     res, 
+//     next
+// ) => { 
+//     // 1. Log request body.
+//     if(!req.url.includes("signin")){
+//         const logData = `${req.url
+//         } - ${JSON.stringify(req.body)}`;
+//         await log(logData);
+//     }
+//     next();
+// };
+
+// export default loggerMiddleware;
+
+
+//Winston logger instead of Normal logger
+//using winston first install --> 'npm i winston'
+
+import winston from 'winston';
+
+const logger= winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta:{ service: 'request-logging'},
+    transports:[
+        new winston.transports.File({filename:'logs.txt'})
+    ]
+})
+
+const loggerMiddleware = async (
+    req, 
+    res, 
+    next
+) => { 
+    // 1. Log request body.
+    if(!req.url.includes("signin")){
+        const logData = `${req.url
+        } - ${JSON.stringify(req.body)}`;
+        logger.info(logData);
+    }
+    next();
+};
+
+export default loggerMiddleware;
